202b626ef4001701a285c809d0d8204e0f959b06
Belal <billy.fcih@yahoo.com>
2018-05-02 04:59:58 +0200

    Add about menu

--- src/scanner/view/NavBar.java
+++ src/scanner/view/NavBar.java
@@ -1,9 +1,12 @@
 package scanner.view;
 
+import javafx.scene.control.Alert;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.stage.FileChooser;
+import javafx.stage.StageStyle;
+
 import java.io.File;
 
 public class NavBar extends MenuBar{
@@ -49,6 +52,15 @@ public class NavBar extends MenuBar{
         run.getItems().addAll(scan, parse, compile);
 
         MenuItem about = new MenuItem("About");
+
+        about.setOnAction(event -> {
+            Alert alert=new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("About");
+            alert.setHeaderText(null);
+            alert.setContentText("Write, edit, scan, parse, and compile your code!");
+            alert.getDialogPane().setStyle("-fx-background-color: #2b2b2b; -fx-base:black;-fx-control-inner-background: #2b2b2b;");
+            alert.showAndWait();
+        });
 
         help.getItems().addAll(about);
 
