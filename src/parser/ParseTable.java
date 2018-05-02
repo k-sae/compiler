d22fa98df354a26738dc4e5675b3f27a19889d26
amr-sameh <amrsameh.develop@gmail.com>
2018-05-02 02:16:46 +0200

    parse table

--- src/parser/ParseTable.java
+++ src/parser/ParseTable.java
@@ -1,16 +1,29 @@
 package parser;
 
+import java.util.ArrayList;
+
 /**
  * Created by amr-sameh on 01/05/18.
  */
 public class ParseTable {
     public static ParseTable instance=null;
-    private ParseTable(){}
+    private ArrayList<ParseTableRow> table;
+
+    private ParseTable(){
+        table = new ArrayList<ParseTableRow>();
+        ArrayList <GrammerRule> rowData =new ArrayList<GrammerRule>();
+
+        rowData.add(new GrammerRule("",new ArrayList<String>()));
+        table.add(new ParseTableRow("Add_Op",rowData));
+    }
     public static ParseTable getInstance(){
         if(instance==null)
             instance = new ParseTable();
         return instance;
     }
+
+
+
     public boolean isTerminal (String s){
         return false;
     }
